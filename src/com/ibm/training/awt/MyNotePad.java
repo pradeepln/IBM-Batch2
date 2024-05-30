637bfaf981f861afe2d0c7c9547df1f806dd5ea5
Pradeep LN <pradeep.ln78@gmail.com>
2024-05-30 17:51:14 +0530

    eod 19

---
+++ src/com/ibm/training/awt/MyNotePad.java
@@ -0,0 +1,116 @@
+package com.ibm.training.awt;
+
+import java.awt.FileDialog;
+import java.awt.Frame;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class MyNotePad extends Frame implements ActionListener {
+
+	String absolutePathToFile;
+	
+	MenuBar mBar = new MenuBar();
+	Menu fileMenu = new Menu("File");
+	MenuItem newItem = new MenuItem("New");
+	MenuItem openItem = new MenuItem("Open");
+	MenuItem saveItem = new MenuItem("Save");
+	MenuItem exitItem = new MenuItem("Exit");
+	
+	TextArea textArea = new TextArea();
+	
+	MyNotePad(){
+		super("My Own Notepad!!");
+		this.setSize(300, 400);
+		this.setLocation(350, 350);
+		
+		fileMenu.add(newItem);
+		fileMenu.add(openItem);
+		fileMenu.add(saveItem);
+		fileMenu.add(exitItem);
+		mBar.add(fileMenu);
+		this.setMenuBar(mBar);
+	
+		this.add(textArea);
+		
+		this.setVisible(true);
+		
+		fileMenu.addActionListener(this);
+		
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+	
+	public static void main(String[] args) {
+		new MyNotePad();
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		String actionCommand = e.getActionCommand();
+		
+		switch(actionCommand) {
+		case "Open":
+			doOpen();
+			break;
+		case "Save":
+			doSave();
+			break;
+		}
+		
+			
+		
+			
+		
+	}
+
+	private void doSave() {
+		
+		String text = textArea.getText();
+		
+		try(BufferedWriter out = new BufferedWriter(new FileWriter(absolutePathToFile));) {
+			out.write(text);
+			out.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void doOpen() {
+		System.out.println("opening");
+		FileDialog fd = new FileDialog(this, "Select File to Open", FileDialog.LOAD);
+		fd.setVisible(true);
+		String directory = fd.getDirectory();
+		String file = fd.getFile();
+		absolutePathToFile = directory+file;
+		
+		textArea.setText("");
+		
+		String line;
+		
+		try(BufferedReader in = new BufferedReader(new FileReader(absolutePathToFile));) {
+			
+			while ((line = in.readLine()) != null) {
+				textArea.append(line + "\n");
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- src/com/ibm/training/awt/SimpleFrameDemo.java
+++ src/com/ibm/training/awt/SimpleFrameDemo.java
@@ -7,6 +7,8 @@ import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.Panel;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
@@ -25,11 +27,22 @@ public class SimpleFrameDemo {
 		TextField tf = new TextField(20);
 		Button btn = new Button("Click Me");
 		
+		TextField status = new TextField(20);
+		status.setEditable(false);
+		
 		panel.add(label);
 		panel.add(tf);
 		panel.add(btn);
+		panel.add(status);
 		
 		frame.add(panel, BorderLayout.CENTER);
+		
+		btn.addActionListener(e -> {
+			String text = tf.getText();
+			status.setText(text);
+			tf.setText("");
+		});
+		
 		
 		//frame.addWindowListener(new MywindowCloser());
 		frame.addWindowListener(new WindowAdapter() {
@@ -45,6 +58,15 @@ public class SimpleFrameDemo {
 	
 
 }
+
+//class MyBtnHandler implements ActionListener{
+//
+//	@Override
+//	public void actionPerformed(ActionEvent e) {
+//		System.out.println(e);		
+//	}
+//	
+//}
 
 //class MywindowCloser extends WindowAdapter{
 //	@Override
